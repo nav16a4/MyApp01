d51b4a5f2c4830dd99f586e55c54a8e38fff8f1c
nav16a4 <blu37in@naver.com>
2024-01-12 02:11:56 +0900

    1.12_0

--- GameEngineBase/MyString.h
+++ GameEngineBase/MyString.h
@@ -1,10 +1,33 @@
 #pragma once
-class MyString
+#include <string>
+
+namespace My
+{
+	namespace String
+	{
+		class MyString;
+	}
+}
+
+
+class My::String::MyString
 {
 public:
+	MyString();
+	MyString(const std::string& cp949);
+	MyString(const std::u8string& utf8);
+
+	MyString(const MyString& str);
+	MyString& operator=(const MyString& str);
+
+
+	MyString(MyString&& str);
+	MyString& operator=(MyString&& str);
+
+	~MyString() = default;
 
 public:
-
+	static void ConvertEncoding(std::string& utf8, const std::string& cp949);
 
 };
 
